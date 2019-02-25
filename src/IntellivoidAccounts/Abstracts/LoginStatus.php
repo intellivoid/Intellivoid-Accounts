d6a47b257b7d4dbdcbf5ed80180db64f4fc6d198
Narrakas <zixing.narrakas1996@gmail.com>
2019-02-25 13:55:37 -0300

    Created \IntellivoidAccounts\Object LoginRecord & \IntellivoidAccounts\Abstracts LoginStatus

---
+++ src/IntellivoidAccounts/Abstracts/LoginStatus.php
@@ -0,0 +1,14 @@
+<?php
+
+    namespace IntellivoidAccounts\Abstracts;
+
+    /**
+     * Class LoginStatus
+     * @package IntellivoidAccounts\Abstracts
+     */
+    abstract class LoginStatus
+    {
+        const Successful = 0;
+
+        const IncorrectCredentials = 1;
+    }
