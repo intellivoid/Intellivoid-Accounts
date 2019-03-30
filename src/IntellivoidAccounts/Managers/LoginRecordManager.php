c825429cbce40ff711e29e522e50790bb39291c1
Narrakas <zixing.narrakas1996@gmail.com>
2019-03-30 18:24:13 -0300

    Added ip() to IntellivoidAccouts\Utilities\Validate.class

--- src/IntellivoidAccounts/Managers/LoginRecordManager.php
+++ src/IntellivoidAccounts/Managers/LoginRecordManager.php
@@ -2,6 +2,7 @@
 
     namespace IntellivoidAccounts\Managers;
 
+    use IntellivoidAccounts\Exceptions\AccountNotFoundException;
     use IntellivoidAccounts\IntellivoidAccounts;
 
     /**
@@ -26,6 +27,12 @@
 
         public function createLoginRecord(string $account_id, string $ip_address, int $status, string $origin)
         {
+            if($this->intellivoidAccounts->getAccountManager()->publicIdExists($account_id) == false)
+            {
+                throw new AccountNotFoundException();
+            }
 
+            $account_id = $this->intellivoidAccounts->database->real_escape_string($account_id);
+            $ip_address = $this->intellivoidAccounts->database->real_escape_string($ip_address);
         }
     }
