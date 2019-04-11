d6496e10b923eec29cb90f34e1640ed3ad52a5cc
narrakas <zixing.narrakas1996@gmail.com>
2019-04-11 17:40:12 -0400

    Added properties to \IntellivoidAccounts\Obejcts\Account\Configuration\OpenBlu.class; CurrentPlan, AccessKeyID, TransactionID, TransactionProcessedTimestamp

--- src/IntellivoidAccounts/Objects/Account/Configuration/OpenBlu.php
+++ src/IntellivoidAccounts/Objects/Account/Configuration/OpenBlu.php
@@ -2,12 +2,45 @@
 
     namespace IntellivoidAccounts\Objects\Account\Configuration;
 
+    use IntellivoidAccounts\Abstracts\OpenBluPlan;
+
     /**
      * Class OpenBlu
      * @package IntellivoidAccounts\Objects\Account\Configuration
      */
     class OpenBlu
     {
+        /**
+         * @var OpenBluPlan|int
+         */
+        public $CurrentPlan;
+
+        /**
+         * The access key ID
+         *
+         * @var int
+         */
+        public $AccessKeyID;
+
+        /**
+         * The PayPal Transaction ID
+         *
+         * @var string
+         */
+        public $TransactionID;
+
+        /**
+         * @var int
+         */
+        public $TransactionProcessedTimestamp;
+
+        /**
+         * OpenBlu constructor.
+         */
+        public function __construct()
+        {
+            $this->CurrentPlan = OpenBluPlan::None;
+        }
 
         /**
          * Converts object to array
@@ -16,7 +49,12 @@
          */
         public function toArray(): array
         {
-            return array();
+            return array(
+                'current_plan' => (int)$this->CurrentPlan,
+                'access_key_id' => (int)$this->AccessKeyID,
+                'transaction_id' => $this->TransactionID,
+                'transaction_processed_timestamp' => (int)$this->TransactionProcessedTimestamp
+            );
         }
 
         /**
@@ -28,6 +66,26 @@
         public static function fromArray(array $data): OpenBlu
         {
             $ConfigurationObject = new OpenBlu();
+
+            if(isset($data['current_plan']))
+            {
+                $ConfigurationObject->CurrentPlan = (int)$data['current_plan'];
+            }
+
+            if(isset($data['access_key_id']))
+            {
+                $ConfigurationObject->AccessKeyID = (int)$data['access_key_id'];
+            }
+
+            if(isset($data['transaction_id']))
+            {
+                $ConfigurationObject->TransactionID = $data['transaction_id'];
+            }
+
+            if(isset($data['transaction_processed_timestamp']))
+            {
+                $ConfigurationObject->TransactionProcessedTimestamp = (int)$data['transaction_processed_timestamp'];
+            }
 
             return $ConfigurationObject;
         }
