0fb1660bb3c449d72a90c8daa3fb7c5b5723b592
Netkas <45276434+Netkas@users.noreply.github.com>
2019-09-21 14:10:04 -0300

    Added method generateLoginCode() to \IntellivoidAccounts\Managers > OtlManager

--- src/IntellivoidAccounts/Managers/OtlManager.php
+++ src/IntellivoidAccounts/Managers/OtlManager.php
@@ -4,8 +4,11 @@
     namespace IntellivoidAccounts\Managers;
 
 
+    use IntellivoidAccounts\Abstracts\OtlStatus;
+    use IntellivoidAccounts\Exceptions\DatabaseException;
     use IntellivoidAccounts\IntellivoidAccounts;
     use IntellivoidAccounts\Utilities\Hashing;
+    use msqg\QueryBuilder;
 
     /**
      * Class OtlManager
@@ -27,6 +30,13 @@
             $this->intellivoidAccounts = $intellivoidAccounts;
         }
 
+        /**
+         * Generates a one-time login code for the given account ID
+         *
+         * @param int $account_id
+         * @return string
+         * @throws DatabaseException
+         */
         public function generateLoginCode(int $account_id): string
         {
             $created_timestamp = (int)time();
@@ -35,6 +45,25 @@
             $code = Hashing::OneTimeLoginCode($account_id, $created_timestamp, $expires_timestamp);
             $code = $this->intellivoidAccounts->database->real_escape_string($code);
             $account_id = (int)$account_id;
-            $status = Otl
+            $status = (int)OtlStatus::Available;
+            $vendor = 'None';
+
+            $Query = QueryBuilder::insert_into('otl_codes', array(
+                'code' => $code,
+                'vendor' => $vendor,
+                'account_id' => $account_id,
+                'status' => $status,
+                'expires' => $expires_timestamp,
+                'created' => $created_timestamp
+            ));
+            $QueryResults = $this->intellivoidAccounts->database->query($Query);
+            if($QueryResults == false)
+            {
+                throw new DatabaseException($Query, $this->intellivoidAccounts->database->error);
+            }
+            else
+            {
+                return $code;
+            }
         }
     }
