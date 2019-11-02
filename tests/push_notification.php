081e465d86c86e180ce5baa88e8d4d57d3b81d81
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-01 23:52:54 -0300

    Minor bug fixes

--- tests/push_notification.php
+++ tests/push_notification.php
@@ -1,14 +1,48 @@
 <?php
 
 
+    use IntellivoidAccounts\Abstracts\SearchMethods\TelegramClientSearchMethod;
+    use IntellivoidAccounts\Exceptions\AuthNotPromptedException;
+use IntellivoidAccounts\Exceptions\AuthPromptDeniedException;
+use IntellivoidAccounts\Exceptions\AuthPromptExpiredException;
+    use IntellivoidAccounts\Exceptions\TelegramServicesNotAvailableException;
+    use IntellivoidAccounts\IntellivoidAccounts;
+
     $SourceFolder = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
     include_once($SourceFolder . 'IntellivoidAccounts' . DIRECTORY_SEPARATOR . 'IntellivoidAccounts.php');
 
-    $IntellivoidAccounts = new \IntellivoidAccounts\IntellivoidAccounts();
+    $IntellivoidAccounts = new IntellivoidAccounts();
 
     $TelegramClient = $IntellivoidAccounts->getTelegramClientManager()->getClient(
-        \IntellivoidAccounts\Abstracts\SearchMethods\TelegramClientSearchMethod::byId, '1'
+        TelegramClientSearchMethod::byId, '1'
     );
 
     //$IntellivoidAccounts->getTelegramService()->sendNotification($TelegramClient, "CoffeeHouse", "Test Notification!");
-    $IntellivoidAccounts->getTelegramService()->promptAuth($TelegramClient, "Netkas");
+    $IntellivoidAccounts->getTelegramService()->promptAuth($TelegramClient, "Netkas", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36", 3);
+
+
+    print("Waiting for authentication\n");
+
+    while(true)
+    {
+        try
+        {
+            if($IntellivoidAccounts->getTelegramService()->pollAuthPrompt($TelegramClient) == true)
+            {
+                print("Authenticated!\n");
+                exit();
+            }
+        }
+        catch(AuthPromptDeniedException $authPromptDeniedException)
+        {
+            print("Denied\n");
+            exit();
+        }
+        catch(Exception $exception)
+        {
+            /** @noinspection PhpUnhandledExceptionInspection */
+            throw $exception;
+        }
+
+        sleep(5);
+    }
