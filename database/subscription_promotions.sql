458135b5001a3b5c939d34c3ab10a86e25b313d9
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-11 09:00:54 -0300

    Updated structure 'subscription_promotions'

--- database/subscription_promotions.sql
+++ database/subscription_promotions.sql
@@ -3,7 +3,7 @@ create table subscription_promotions
     id                        int(255) auto_increment,
     public_id                 varchar(255) null comment 'Unique public ID for this subscription promotion',
     promotion_code            varchar(255) null comment 'User friendly promotion code',
-    application_id            int(255)     null comment 'The Application ID that this promotion code is affiliated with',
+    subscription_plan_id      int(255)     null comment 'The subscription plan ID that this promotion is applicable to',
     affiliation_account_id    int(255)     null comment 'THe Account ID that receives affiliations, 0 = None',
     affiliation_initial_share float        null comment 'The amount of the initial price to share with the affiliation, 0 = None',
     affiliation_cycle_share   float        null comment 'The amount to share per cycle with the affiliation, 0 = None',
