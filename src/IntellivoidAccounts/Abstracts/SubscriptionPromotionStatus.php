c88a9d19c8155a0a6867caf4187ba8c060b4d00d
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-11 00:06:13 -0300

    Added missing docs

--- src/IntellivoidAccounts/Abstracts/SubscriptionPromotionStatus.php
+++ src/IntellivoidAccounts/Abstracts/SubscriptionPromotionStatus.php
@@ -9,9 +9,18 @@
      */
     abstract class SubscriptionPromotionStatus
     {
+        /**
+         * Indicates if this promotion code is active
+         */
         const Active = 0;
 
+        /**
+         * Indicates if this promotion code is disabled
+         */
         const Disabled = 1;
 
+        /**
+         * Indicates if this promotion code is deleted
+         */
         const Deleted = 2;
     }
