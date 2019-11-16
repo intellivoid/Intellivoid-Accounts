6a7c0505b828622fd5077c12acf81f947462f54b
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-15 22:12:31 -0300

    Added method subscriptionPromotionCode() to \IntellivoidAccounts\Utilities > Validate

--- src/IntellivoidAccounts/Utilities/Validate.php
+++ src/IntellivoidAccounts/Utilities/Validate.php
@@ -322,4 +322,30 @@
 
             return false;
         }
+
+        /**
+         * Validates if the promotion code for the subscription is valid or not
+         *
+         * @param string $input
+         * @return bool
+         */
+        public static function subscriptionPromotionCode(string $input): bool
+        {
+            if(strlen($input) > 120)
+            {
+                return false;
+            }
+
+            if(strlen($input) < 3)
+            {
+                return false;
+            }
+
+            if(preg_match("/^[a-zA-Z0-9 ]*$/", $input))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
