bec89ea89c6b026297aa272adb4e24683f2e9205
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-15 22:06:51 -0300

    Added method SubscriptionPromotionPublicID() to \IntellivoidAccounts\Utilities > Hashing

--- src/IntellivoidAccounts/Managers/SubscriptionPromotionManager.php
+++ src/IntellivoidAccounts/Managers/SubscriptionPromotionManager.php
@@ -5,6 +5,8 @@
 
 
     use IntellivoidAccounts\IntellivoidAccounts;
+    use IntellivoidAccounts\Objects\SubscriptionPromotion;
+    use IntellivoidAccounts\Utilities\Hashing;
 
     /**
      * Class SubscriptionPromotionManager
@@ -24,5 +26,10 @@
         public function __construct(IntellivoidAccounts $intellivoidAccounts)
         {
             $this->intellivoidAccounts = $intellivoidAccounts;
+        }
+
+        public function createSubscriptionPromotion(int $subscription_plan_id, string $promotion_code, int $affiliation_account_id, float $affiliation_initial_share, float $affiliation_cycle_share, array $features): SubscriptionPromotion
+        {
+            $public_id = Hashing::SubscriptionPromotionPublicID((int)$subscription_plan_id, $promotion_code);
         }
     }
--- src/IntellivoidAccounts/Utilities/Hashing.php
+++ src/IntellivoidAccounts/Utilities/Hashing.php
@@ -402,4 +402,16 @@
         {
             return hash('crc32b', $application_id) . hash('sha256', $plan_name);
         }
+
+        /**
+         * Calculates a unqiue publid ID for the subscription
+         *
+         * @param int $subscription_plan_id
+         * @param string $promotion_code
+         * @return string
+         */
+        public static function SubscriptionPromotionPublicID(int $subscription_plan_id, string $promotion_code): string
+        {
+            return hash('crc32b', $subscription_plan_id) . hash('sha256', $promotion_code);
+        }
     }
