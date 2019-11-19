93fea1c631254a972c90db5fe6ed393c0b03c5d0
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-19 16:55:37 -0300

    Added method logTransaction() to \IntellivoidAccounts\Managers

--- src/IntellivoidAccounts/Managers/TransactionRecordManager.php
+++ src/IntellivoidAccounts/Managers/TransactionRecordManager.php
@@ -4,7 +4,10 @@
     namespace IntellivoidAccounts\Managers;
 
 
+    use IntellivoidAccounts\Exceptions\DatabaseException;
     use IntellivoidAccounts\IntellivoidAccounts;
+    use IntellivoidAccounts\Utilities\Hashing;
+    use msqg\QueryBuilder;
 
     /**
      * Class TransactionRecordManager
@@ -26,8 +29,37 @@
             $this->intellivoidAccounts = $intellivoidAccounts;
         }
 
+        /**
+         * Logs the transaction
+         *
+         * @param int $account_id
+         * @param string $vendor
+         * @param float $amount
+         * @return bool
+         * @throws DatabaseException
+         */
         public function logTransaction(int $account_id, string $vendor, float $amount): bool
         {
+            $timestamp = (int)time();
+            $public_id = Hashing::TransactionRecordPublicID($account_id, $vendor, $timestamp);
+            $account_id = (int)$account_id;
+            $vendor = $this->intellivoidAccounts->database->real_escape_string($vendor);;
+            $amount = (float)$amount;
 
+            $Query = QueryBuilder::insert_into('transaction_records', array(
+                'public_id' => $public_id,
+                'account_id' => $account_id,
+                'vendor' => $vendor,
+                'amount' => $amount,
+                'timestamp' => $timestamp
+            ));
+            $QueryResults = $this->intellivoidAccounts->database->query($Query);
+
+            if($QueryResults == false)
+            {
+                throw new DatabaseException($Query, $this->intellivoidAccounts->database->error);
+            }
+
+            return true;
         }
     }
