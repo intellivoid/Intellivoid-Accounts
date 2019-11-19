769b629a39ee68dfa77fd4dedc4fa3e14c360856
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-19 18:25:58 -0300

    Added method addFunds() to \IntellivoidAccounts\Managers > TransactionManager

--- src/IntellivoidAccounts/Managers/TransactionManager.php
+++ src/IntellivoidAccounts/Managers/TransactionManager.php
@@ -4,10 +4,16 @@
     namespace IntellivoidAccounts\Managers;
 
 
+    use BasicCalculator\BC;
+    use IntellivoidAccounts\Abstracts\AccountStatus;
     use IntellivoidAccounts\Abstracts\SearchMethods\AccountSearchMethod;
     use IntellivoidAccounts\Exceptions\AccountNotFoundException;
     use IntellivoidAccounts\Exceptions\DatabaseException;
+    use IntellivoidAccounts\Exceptions\InvalidAccountStatusException;
+    use IntellivoidAccounts\Exceptions\InvalidEmailException;
+    use IntellivoidAccounts\Exceptions\InvalidFundsValueException;
     use IntellivoidAccounts\Exceptions\InvalidSearchMethodException;
+    use IntellivoidAccounts\Exceptions\InvalidUsernameException;
     use IntellivoidAccounts\Exceptions\InvalidVendorException;
     use IntellivoidAccounts\IntellivoidAccounts;
     use IntellivoidAccounts\Utilities\Validate;
@@ -37,20 +43,41 @@
          *
          * @param int $account_id
          * @param string $vendor
-         * @param float $funds
+         * @param float $amount
          * @return bool
          * @throws AccountNotFoundException
          * @throws DatabaseException
+         * @throws InvalidFundsValueException
          * @throws InvalidSearchMethodException
          * @throws InvalidVendorException
+         * @throws InvalidAccountStatusException
+         * @throws InvalidEmailException
+         * @throws InvalidUsernameException
          */
-        public function addFunds(int $account_id, string $vendor, float $funds): bool
+        public function addFunds(int $account_id, string $vendor, float $amount): bool
         {
             if(Validate::vendor($vendor) == false)
             {
                 throw new InvalidVendorException();
             }
 
+            if($amount < 0)
+            {
+                throw new InvalidFundsValueException();
+            }
+
             $Account = $this->intellivoidAccounts->getAccountManager()->getAccount(AccountSearchMethod::byId, $account_id);
+            $Account->Configuration->Balance = (float)BC::add($Account->Configuration->Balance, abs($amount), 2);
+            $this->intellivoidAccounts->getAccountManager()->updateAccount($Account);
+            $this->intellivoidAccounts->getTransactionRecordManager()->logTransaction(
+                $Account->ID, $vendor, $amount
+            );
+            
+            return True;
+        }
+
+        public function processPayment(int $account_id, string $vendor, float $amount): bool
+        {
+
         }
     }
