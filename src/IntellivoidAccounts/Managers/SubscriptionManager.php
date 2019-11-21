24d49d02882f5d10ced0d7872306aedb13eae283
Netkas <45276434+Netkas@users.noreply.github.com>
2019-11-20 23:31:47 -0300

    Added method cancelSubscription() to \IntellivoidAccounts\Managers > SubscriptionManager

--- src/IntellivoidAccounts/Managers/SubscriptionManager.php
+++ src/IntellivoidAccounts/Managers/SubscriptionManager.php
@@ -330,4 +330,28 @@
 
             return True;
         }
+
+        /**
+         * Cancels an existing subscription
+         *
+         * @param Subscription $subscription
+         * @return bool
+         * @throws DatabaseException
+         */
+        public function cancelSubscription(Subscription $subscription)
+        {
+            $id = (int)$subscription->ID;
+
+            $Query = "DELETE FROM `subscriptions` WHERE id=$id";
+            $QueryResults = $this->intellivoidAccounts->database->query($Query);
+
+            if($QueryResults == true)
+            {
+                return true;
+            }
+            else
+            {
+                throw new DatabaseException($Query, $this->intellivoidAccounts->database->error);
+            }
+        }
     }
