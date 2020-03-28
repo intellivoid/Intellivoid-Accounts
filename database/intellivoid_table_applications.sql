3d9e1393db105d4a44d671fbcdf4555f3ed92967
netkas <netkas@intellivoid.info>
2020-03-28 18:35:00 -0300

    Updated structure 'applications' to use signed signatures

--- database/intellivoid_table_applications.sql
+++ database/intellivoid_table_applications.sql
@@ -1,21 +1,33 @@
+create table if not exists intellivoid.applications
+(
+    id                     int auto_increment comment 'Internal Database ID',
+    public_app_id          varchar(255) null comment 'The public unique Application ID',
+    secret_key             varchar(255) null comment 'The secret key for authentication operations',
+    name                   varchar(255) null comment 'The name of the application',
+    name_safe              varchar(255) null comment 'Safe version of the application name',
+    permissions            blob         null comment 'The permissions that this appication requires when requesting authentication access',
+    status                 int          null comment 'The status of this application, this determines the operation of the authentication',
+    authentication_mode    int          null comment 'The mode of authentication that this application uses',
+    account_id             int          null comment 'The account ID that this application is owned by',
+    flags                  blob         null comment 'Flags associated with this Application',
+    creation_timestamp     int          null comment 'The Unix Timestamp of when this application was registered',
+    last_updated_timestamp int          null comment 'The Unix Timestamp of when this application was last updated',
+    constraint applications_id_uindex
+        unique (id),
+    constraint applications_name_name_safe_uindex
+        unique (name, name_safe),
+    constraint applications_name_safe_uindex
+        unique (name_safe),
+    constraint applications_name_uindex
+        unique (name),
+    constraint applications_public_app_id_uindex
+        unique (public_app_id)
+)
+    comment 'Table for Applications that are registered in Intellivoid' collate = utf8mb4_general_ci;
 
--- --------------------------------------------------------
+create index applications_public_app_id_secret_key_index
+    on intellivoid.applications (public_app_id, secret_key);
 
---
--- Table structure for table `applications`
---
+alter table intellivoid.applications
+    add primary key (id);
 
-CREATE TABLE `applications` (
-  `id` int(255) NOT NULL COMMENT 'Internal Database ID',
-  `public_app_id` varchar(255) DEFAULT NULL COMMENT 'The public unique Application ID',
-  `secret_key` varchar(255) DEFAULT NULL COMMENT 'The secret key for authentication operations',
-  `name` varchar(255) DEFAULT NULL COMMENT 'The name of the application',
-  `name_safe` varchar(255) DEFAULT NULL COMMENT 'Safe version of the application name',
-  `permissions` blob DEFAULT NULL COMMENT 'The permissions that this appication requires when requesting authentication access',
-  `status` int(255) DEFAULT NULL COMMENT 'The status of this application, this determines the operation of the authentication',
-  `authentication_mode` int(255) DEFAULT NULL COMMENT 'The mode of authentication that this application uses',
-  `account_id` int(255) DEFAULT NULL COMMENT 'The account ID that this application is owned by',
-  `flags` blob DEFAULT NULL COMMENT 'Flags associated with this Application',
-  `creation_timestamp` int(255) DEFAULT NULL COMMENT 'The Unix Timestamp of when this application was registered',
-  `last_updated_timestamp` int(255) DEFAULT NULL COMMENT 'The Unix Timestamp of when this application was last updated'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table for Applications that are registered in Intellivoid';
