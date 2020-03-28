dc030e9838464b295e6a5965ab20983cb028ad08
netkas <netkas@intellivoid.info>
2020-03-28 18:44:41 -0300

    Updated structure 'users_audit' to use signed signatures

--- database/intellivoid_table_users_audit.sql
+++ database/intellivoid_table_users_audit.sql
@@ -1,13 +1,14 @@
+create table if not exists intellivoid.users_audit
+(
+    id         int auto_increment,
+    account_id int null comment 'The ID of the Account associated with this event',
+    event_type int null comment 'The type of event',
+    timestamp  int null comment 'The timestamp of when this record was created',
+    constraint users_audit_id_uindex
+        unique (id)
+)
+    comment 'Security log for account changes' collate = utf8mb4_general_ci;
 
--- --------------------------------------------------------
+alter table intellivoid.users_audit
+    add primary key (id);
 
---
--- Table structure for table `users_audit`
---
-
-CREATE TABLE `users_audit` (
-  `id` int(255) NOT NULL,
-  `account_id` int(255) DEFAULT NULL COMMENT 'The ID of the Account associated with this event',
-  `event_type` int(255) DEFAULT NULL COMMENT 'The type of event',
-  `timestamp` int(255) DEFAULT NULL COMMENT 'The timestamp of when this record was created'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Security log for account changes';
