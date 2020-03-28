056b911d54ec73e4d03a3792fe6181cfcdc960f5
netkas <netkas@intellivoid.info>
2020-03-28 18:43:34 -0300

    Updated structure 'transaction_records' to use signed signatures

--- database/intellivoid_table_transaction_records.sql
+++ database/intellivoid_table_transaction_records.sql
@@ -1,15 +1,23 @@
+create table if not exists intellivoid.transaction_records
+(
+    id         int auto_increment comment 'Unique internal database ID for this transaction record',
+    public_id  varchar(255) null comment 'Unique public ID for this transaction record',
+    account_id int          null comment 'The account ID that this transaction is associated with',
+    vendor     varchar(255) null comment 'The name of the vendor or account username that this transaction is to or from',
+    amount     float        null comment 'The amount that the associated account has withdrawn or received
 
--- --------------------------------------------------------
+< 0 	= Currency taken from associated account
+0 	= No currency taken
+> 0 	= Currency added to the associated account ',
+    timestamp  int          null comment 'The Unix Timestamp of when this transaction took place',
+    constraint transaction_records_id_uindex
+        unique (id),
+    constraint transaction_records_public_id_uindex
+        unique (public_id)
+)
+    comment 'Records of Balance Transactions regarding subscription payments, payments, withdrawals, etc.'
+    collate = utf8mb4_general_ci;
 
---
--- Table structure for table `transaction_records`
---
+alter table intellivoid.transaction_records
+    add primary key (id);
 
-CREATE TABLE `transaction_records` (
-  `id` int(255) NOT NULL COMMENT 'Unique internal database ID for this transaction record',
-  `public_id` varchar(255) DEFAULT NULL COMMENT 'Unique public ID for this transaction record',
-  `account_id` int(255) DEFAULT NULL COMMENT 'The account ID that this transaction is associated with',
-  `vendor` varchar(255) DEFAULT NULL COMMENT 'The name of the vendor or account username that this transaction is to or from',
-  `amount` float DEFAULT NULL COMMENT 'The amount that the associated account has withdrawn or received\r\n\r\n< 0 	= Currency taken from associated account\r\n0 	= No currency taken\r\n> 0 	= Currency added to the associated account ',
-  `timestamp` int(255) DEFAULT NULL COMMENT 'The Unix Timestamp of when this transaction took place'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Records of Balance Transactions regarding subscription payments, payments, withdrawals, etc.';
